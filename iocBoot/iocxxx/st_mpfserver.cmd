73f7017887974bb1859daf04a6465c806be43448
MarkRivers <rivers@cars.uchicago.edu>
2004-01-18 18:17:44 +0000

    Numerous changes, including: Fixed IpUnidig Example of multiple carriers Made more of serial ports be consistent with st.cmd, but not all Support for MPC Changed initIp330PID Removed configIp330PID, no longer exists

--- iocBoot/iocxxx/st_mpfserver.cmd
+++ iocBoot/iocxxx/st_mpfserver.cmd
@@ -1,9 +1,14 @@
 # This configures the MPF server stuff
 
+# First carrier
 # slot a: IP-Octal (serial RS-232)
-# slot b: EMPTY
-# slot c: EMPTY
-# slot d: EMPTY
+# slot b: IP-488 (GPIB)
+# slot c: Dac128V (D/A converter)
+# slot d: Ip330 (A/D converter)
+
+# Second carrier
+# slot a: IpUnidig (digital I/O)
+# slot b: IP-Octal (serial RS-232)
 
 ###############################################################################
 # Initialize IP carrier
@@ -12,24 +17,28 @@
 #   cardParams - carrier-specific init parameters
 
 # Select for MVME162 or MVME172 CPU board IP carrier.
-carrier = "mv162"
+carrier1 = "mv162"
 ipacAddCarrier(&ipmv162, "A:l=3,3 m=0xe0000000,64;B:l=3,3 m=0xe0010000,64;C:l=3,3 m=0xe0020000,64;D:l=3,3 m=0xe0030000,64")
 
 # Select for SBS VIPC616-01 version IP carrier.
-#carrier = "VIPC616_01"
-#ipacAddCarrier(&vipc616_01, "0x6000,D00000,128")
+#carrier1 = "VIPC616_01"
+#ipacAddCarrier(&vipc616_01, "0x3000,D00000,128")
+#carrier2 = "VIPC616_02"
+#ipacAddCarrier(&vipc616_01, "0x3400,D00000,128")
 
-initIpacCarrier(carrier, 0)
+initIpacCarrier(carrier1, 0)
+#initIpacCarrier(carrier2, 0)
 
 ###############################################################################
 # Initialize GPIB stuff
 
-#initGpibGsTi9914("GS-IP488-0",carrier,"IP_b",104)
+#initGpibGsTi9914("GS-IP488-0",carrier1,"IP_b",104)
 #initGpibServer("GPIB0","GS-IP488-0",1024,1000)
 
 ###############################################################################
 # Initialize Octal UART stuff
-initOctalUART("octalUart0",carrier,"IP_a",8,100)
+initOctalUART("octalUart0",carrier1,"IP_a",8,100)
+#initOctalUART("octalUart1",carrier2,"IP_b",8,100)
 
 # initOctalUARTPort(char* portName,char* moduleName,int port,int baud,
 #                   char* parity,int stop_bits,int bits_char,char* flow_control)
@@ -41,35 +50,58 @@ initOctalUART("octalUart0",carrier,"IP_a",8,100)
 #
 # Don't leave any of these uninitialized, even if you aren't going to use them.
 
-initOctalUARTPort("UART[0]","octalUart0",0, 9600,"N",2,8,"N") /* SRS570 */
-initOctalUARTPort("UART[1]","octalUart0",1, 9600,"N",2,8,"N") /* SRS570 */
-initOctalUARTPort("UART[2]","octalUart0",2, 9600,"N",2,8,"N") /* SRS570 */
-initOctalUARTPort("UART[3]","octalUart0",3, 9600,"N",2,8,"N") /* SRS570 */
-initOctalUARTPort("UART[4]","octalUart0",4, 9600,"N",2,8,"N") /* ? */
-initOctalUARTPort("UART[5]","octalUart0",5, 9600,"N",2,8,"N") /* ? */
-initOctalUARTPort("UART[6]","octalUart0",6, 9600,"N",2,8,"N") /* ? */
-initOctalUARTPort("UART[7]","octalUart0",7, 9600,"N",1,8,"N") /* Keithley 2K */
+initOctalUARTPort("UART[0]","octalUart0",0, 9600,"N",2,8,"N")  /* SRS570 */
+initOctalUARTPort("UART[1]","octalUart0",1,19200,"E",1,8,"N")  /* MKS */
+initOctalUARTPort("UART[2]","octalUart0",2, 9600,"E",1,7,"N")  /* Digitel */
+initOctalUARTPort("UART[3]","octalUart0",3, 9600,"N",1,8,"N")  /* MPC */
+initOctalUARTPort("UART[4]","octalUart0",4, 9600,"E",1,7,"N")  /* McClennan PM304 */
+initOctalUARTPort("UART[5]","octalUart0",5,19200,"N",1,8,"N")  /* Keithley 2000 */
+initOctalUARTPort("UART[6]","octalUart0",6, 9600,"N",1,8,"N")  /* Oxford ILM cryometer */
+initOctalUARTPort("UART[7]","octalUart0",7,19200,"N",1,8,"N")  /* Love controllers */
+# Second IP-Octal
+initOctalUARTPort("UART[8]", "octalUart1",0,38400,"N",1,8,"N")  /* MM4000 */
+initOctalUARTPort("UART[9]", "octalUart1",1, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[10]","octalUart1",2, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[11]","octalUart1",3, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[12]","octalUart1",4, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[13]","octalUart1",5, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[14]","octalUart1",6, 9600,"N",1,8,"N")  /* ? */
+initOctalUARTPort("UART[15]","octalUart1",7, 9600,"N",1,8,"N")  /* ? */
 
 initSerialServer("a-Serial[0]","UART[0]",1000,20,"\r",1)
 initSerialServer("a-Serial[1]","UART[1]",1000,20,"\r",1)
 initSerialServer("a-Serial[2]","UART[2]",1000,20,"\r",1)
-initSerialServer("a-Serial[3]","UART[3]",1000,20,"\r",1)
+# Comment out the following line if using the MPC, it needs a special sever
+#initSerialServer("a-Serial[3]","UART[3]",1000,20,"\r",1)
+# a-Serial[3] uses the MPC server and needs a larger queue
+initMPCServer("a-Serial[3]","UART[3]",60)
 initSerialServer("a-Serial[4]","UART[4]",1000,20,"\r",1)
 initSerialServer("a-Serial[5]","UART[5]",1000,20,"\r",1)
 initSerialServer("a-Serial[6]","UART[6]",1000,20,"\r",1)
-initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
+# Comment out the following line if using the love controller
+#initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
+initLoveServer("PORT7","UART[7]",112)
 
-###############################################################################
+initSerialServer("b-Serial[0]","UART[8]",1000,20,"\r",1)
+initSerialServer("b-Serial[1]","UART[9]",1000,20,"\r",1)
+initSerialServer("b-Serial[2]","UART[10]",1000,20,"\r",1)
+initSerialServer("b-Serial[3]","UART[11]",1000,20,"\r",1)
+initSerialServer("b-Serial[4]","UART[12]",1000,20,"\r",1)
+initSerialServer("b-Serial[5]","UART[13]",1000,20,"\r",1)
+initSerialServer("b-Serial[6]","UART[14]",1000,20,"\r",1)
+initSerialServer("b-Serial[7]","UART[15]",1000,20,"\r",1)
+
+#
 # Initialize Systran DAC
-# initDAC128V(char *serverName, char *carrierName, char *siteName,
+# pDAC128V = initDAC128V(char *serverName, char *carrierName, char *siteName,
 #             int queueSize)
+# pDAC128V    = pointer to MPF server, used for fast feedback
 # serverName  = name to give this server
 # carrierName = name of IPAC carrier from initIpacCarrier above
 # siteName    = name of IP site, e.g. "IP_a"
 # queueSize   = size of output queue for EPICS
 #
-
-#initDAC128V("c-DAC",carrier,"IP_c",20)
+#pDAC128V = initDAC128V("c-DAC",carrier1,"IP_c",20)
 
 ###############################################################################
 # Initialize Acromag ADC
@@ -97,7 +129,7 @@ initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
 #               10 should certainly be safe.
 # intVec      = Interrupt vector
 
-#pIp330 = initIp330("d-ADC", carrier, "IP_d", "D", "-10to10", 0, 15, 10, 205)
+#pIp330 = initIp330("d-ADC", carrier1, "IP_d", "D", "-10to10", 0, 15, 10, 205)
 
 # int configIp330(Ip330 *pIp330, scanModeType scanMode,
 #    const char *triggerString, int microSecondsPerScan,
@@ -138,7 +170,6 @@ initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
 
 #initIp330Scan(pIp330,"d-Ip330Scan",0,15,100,20)
 
-
 # initIp330Sweep(Ip330 *pIp330, char *serverName, int firstChan, int lastChan,
 #                int maxPoints, int queueSize)
 # pIp330     = pointer returned by initIp330 above
@@ -151,38 +182,46 @@ initSerialServer("a-Serial[7]","UART[7]",1000,20,"\r",1)
 #              allocated will be maxPoints*(lastChan-firstChan+1)*4 bytes
 # queueSize  = size of output queue for EPICS
 
-#initIp330Sweep(pIp330,"d-Ip330Sweep",0,12,2048,20)
+#initIp330Sweep(pIp330,"d-Ip330Sweep",0,12,2048,200)
 
-
-# initIp330PID(char *serverName, Ip330 *pIp330, int ADCChan, 
-#              DAC128V *pDAC128V, int DACChan,
-#              double KP, double KI, double KD,
-#              int interval, int feedbackOn, int lowLimit, int highLimit,
-#              int queueSize)
+#       Ip330 *pIp330, int ADCChannel, DAC128V *pDAC128V, int DACChannel,
+#        int queueSize)
 # serverName = name to give this server
 # pIp330     = pointer returned by initIp330 above
-# ADCChan    = ADC channel to be used by Ip330PID.  This must be in the range
-#              firstChan to lastChan specified in initIp330
-# pDAC128V   = pointer returned by initDAC128V above
-# DACChan    = DAC channel to be used by Ip330PID, in the range 0 to 7
-# KP         = proportional gain, must be a floating point value
-# KI         = integral gain, must be a floating point value
-# KD         = derivative gain, must be a floating point value
-# interval   = number of microseconds per feedback loop
-# feedbackOn = initial state of feedback. 0 = off, 1 = on
-# lowLimit   = lower limit on the DAC output, in the range 0-2047
-# highLimit  = upper limit on the DAC output, in the range 0-2047
+# ADCChannel = ADC channel to be used by Ip330PID as its readback source.
+#              This must be in the range firstChan to lastChan specified in
+#              initIp330
+# pDAC128V   = pointer returned by initDAC128V
+# DACChannel = DAC channel to be used by Ip330PID as its control output.  This
+#              must be in the range 0-7.
 # queueSize  = size of output queue for EPICS
-
 #pIp330PID = initIp330PID("Ip330PID_1", pIp330, 0, pDAC128V, 0, 20)
-#configIp330PID(pIp330PID, .1, 10., 0., 1000, 0, 500, 1500)
 
 ###############################################################################
-# Initialize Greenspring IP-Unidig
-
-#initIpUnidig("b-Unidig", carrier, "IP_b", 20)
-
-# Love controllers
-#initOctalUARTPort("UART[7]","octalUart0",7,19200,"N",1,8,"N")
-#initLoveServer("PORT7","UART[7]",112)
+# Initialize Greenspring IP-Unidig on second carrier board
+# pIpUnidig = initIpUnidig(char *serverName,
+#                          char *carrierName,
+#                          char *siteName,
+#                          int queueSize,
+#                          int msecPoll,
+#                          int intVec,
+#                          int risingMask,
+#                          int fallingMask,
+#                          int biMask,
+#                          int maxClients)
+# iPiUnidig   = pointer to MPF server, used for quedEM interrupts
+# serverName  = name to give this server
+# carrierName = name of IPAC carrier from initIpacCarrier above
+# siteName    = name of IP site, e.g. "IP_a"
+# queueSize   = size of output queue for EPICS
+# msecPoll    = polling time for input bits in msec.  Default=100.
+# intVec      = interrupt vector
+# risingMask  = mask of bits to generate interrupts on low to high (24 bits)
+# fallingMask = mask of bits to generate interrupts on high to low (24 bits)
+# biMask      = mask of bits to generate callbacks to bi record support
+#               This can be a subset of (risingMask | fallingMask)
+# maxClients  = Maximum number of callback tasks which will attach to this
+#               IpUnidig server.  This does not refer to the number of EPICS clients.  
+#               A value of 10 should certainly be safe.
+#pIpUnidig = initIpUnidig("a-Unidig", carrier2, "IP_a", 20, 2000, 116, 1, 1, 0xffff, 10)
 
