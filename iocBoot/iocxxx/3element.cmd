027b3514ab5fa8d19623fb29c6706a9af55f14c2
MarkRivers <rivers@cars.uchicago.edu>
2004-02-16 23:09:50 +0000

    Deleted iocxxx directory, replaced with iocvxWorks

--- iocBoot/iocxxx/3element.cmd
+++
@@ -1,8 +0,0 @@
-# AIMConfig(serverName, int etherAddr, int port, int maxChans, 
-#	int maxSignals, int maxSequences, etherDev, queueSize)
-AIMConfig("AIM1/1", 0xa78, 1, 2048, 1, 1, "ei0", 100)
-AIMConfig("AIM1/2", 0xa78, 2, 2048, 1, 1, "ei0", 100)
-AIMConfig("AIM2/1", 0xa79, 1, 2048, 1, 1, "ei0", 100)
-
-dbLoadRecords("mcaApp/Db/3element.db","P=xxx:med:,N=2000", mca)
-dbLoadTemplate("3element.substitutions", mca)
--- iocBoot/iocxxx/MPFconfig.cmd
+++
@@ -1,6 +0,0 @@
-# For use with dual CPU board configurations where
-# one board is the EPICS processor and the other
-# board is the MPF server processor.
-# Set "Remote_IP" to the MPF server processor.
-Remote_IP = "#.#.#.#"
-
