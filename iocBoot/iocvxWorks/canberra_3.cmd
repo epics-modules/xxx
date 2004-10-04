88c034f245ac391643b35481eb52d1618b316f2d
MarkRivers <rivers@cars.uchicago.edu>
2004-10-04 03:45:24 +0000

    Renamed from 3element.*

---
+++ iocBoot/iocvxWorks/canberra_3.cmd
@@ -0,0 +1,8 @@
+# AIMConfig(serverName, int etherAddr, int port, int maxChans, 
+#	int maxSignals, int maxSequences, etherDev, queueSize)
+AIMConfig("AIM1/1", 0xa78, 1, 2048, 1, 1, "ei0", 100)
+AIMConfig("AIM1/2", 0xa78, 2, 2048, 1, 1, "ei0", 100)
+AIMConfig("AIM2/1", 0xa79, 1, 2048, 1, 1, "ei0", 100)
+
+dbLoadRecords("$(MCA)/mcaApp/Db/3element.db","P=xxx:med:,N=2000")
+dbLoadTemplate("3element.substitutions")
