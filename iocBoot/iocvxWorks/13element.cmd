8aa2157393b2ca7c9f9313d52410299617a30dd1
MarkRivers <rivers@cars.uchicago.edu>
2004-10-04 03:50:58 +0000

    Renamed

--- iocBoot/iocvxWorks/13element.cmd
+++
@@ -1,9 +0,0 @@
-# AIMConfig(serverName, int etherAddr, int port, int maxChans, 
-#	int maxSignals, int maxSequences, etherDev, queueSize)
-AIMConfig("AIM2/1", 0x98c, 1, 4000, 4, 1, "ei0", 100)
-AIMConfig("AIM2/2", 0x98d, 2, 4000, 4, 1, "ei0", 100)
-AIMConfig("AIM3/1", 0x98b, 1, 4000, 4, 1, "ei0", 100)
-AIMConfig("AIM3/2", 0x903, 2, 4000, 4, 1, "ei0", 100)
-
-dbLoadRecords("$(MCA)/mcaApp/Db/13element.db","P=xxx:med:,N=2000")
-dbLoadTemplate("13element.substitutions")
