6eb45d95a0142786e3ce73354bc5d1a993c9cf37
MarkRivers <rivers@cars.uchicago.edu>
2005-03-24 22:42:19 +0000

    Fixed capitalization on ipUnidig.substitutions

--- iocBoot/iocvxWorks/ipUnidig.cmd
+++ iocBoot/iocvxWorks/ipUnidig.cmd
@@ -21,6 +21,6 @@
 initIpUnidig("Unidig1", 0, 1, 2000, 116, 0xfffffb, 0xfffffb)
 
 # IP-Unidig binary I/O
-dbLoadTemplate "IpUnidig.substitutions"
+dbLoadTemplate "ipUnidig.substitutions"
 
 # END ipUnidig.cmd ------------------------------------------------------------
