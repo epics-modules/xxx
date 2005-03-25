546906d9a880597b541af46b67b81c59a997965b
timmmooney <mooney@aps.anl.gov>
2005-03-25 16:56:27 +0000

    New icbConfig() args, substitutions file renamed

--- iocBoot/iocvxWorks/canberra_13.cmd
+++ iocBoot/iocvxWorks/canberra_13.cmd
@@ -17,7 +17,42 @@ AIMConfig("AIM3/1", 0x98b, 1, 4000, 4, 1, "ei0")
 AIMConfig("AIM3/2", 0x903, 2, 4000, 4, 1, "ei0")
 
 dbLoadRecords("$(MCA)/mcaApp/Db/13element.db","P=xxx:med:")
-dbLoadTemplate("canberra_13Element.substitutions")
+
+# icbConfig(portName, ethernetAddress, icbAddress, moduleType)
+#    portName to give to this asyn port
+#    ethernetAddress - Ethernet address of module, low order 16 bits
+#    icbAddress - rotary switch setting inside ICB module
+#    moduleType
+#       0 = ADC
+#       1 = Amplifier
+#       2 = HVPS
+#       3 = TCA
+#       4 = DSP
+# Amp's
+icbConfig("icbAmp1", 0x98c, 1, 1)
+icbConfig("icbAmp2", 0x98c, 2, 1)
+icbConfig("icbAmp3", 0x98c, 3, 1)
+icbConfig("icbAmp4", 0x98c, 4, 1)
+icbConfig("icbAmp5", 0x98c, 5, 1)
+icbConfig("icbAmp6", 0x98c, 6, 1)
+icbConfig("icbAmp7", 0x98c, 8, 1)
+icbConfig("icbAmp8", 0x98c, 9, 1)
+icbConfig("icbAmp9", 0x98b, 1, 1)
+icbConfig("icbAmp10", 0x98b, 2, 1)
+icbConfig("icbAmp11", 0x98b, 3, 1)
+icbConfig("icbAmp12", 0x98b, 4, 1)
+icbConfig("icbAmp13", 0x98b, 5, 1)
+
+# ADC's
+icbConfig("icbAdc1", 0x98c, B, 0)
+icbConfig("icbAdc2", 0x98c, 0, 0)
+icbConfig("icbAdc2", 0x98b, B, 0)
+icbConfig("icbAdc2", 0x98b, 7, 0)
+
+#HVPS's
+icbConfig("icbHvps1", 0x98b, A, 2)
+
+dbLoadTemplate("canberra_13.substitutions")
 
 # END canberra_13.cmd ---------------------------------------------------------
 
