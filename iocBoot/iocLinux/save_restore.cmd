f31e7fe1a5ab2dc95f3aaec784fe69eddcd9106b
timmmooney <mooney@aps.anl.gov>
2008-03-11 16:03:57 +0000

    Added modbus, pilatus, and vac to requestfile path Added call to save_restoreSet_UseStatusPVs() Added macro DEAD_SECONDS for save_restoreStatus.db

--- iocBoot/iocLinux/save_restore.cmd
+++ iocBoot/iocLinux/save_restore.cmd
@@ -1,29 +1,31 @@
+
+# BEGIN save_restore.cmd ------------------------------------------------------
+
 ### save_restore setup
 #
-# The rest this file does not require modification for standard use, but...
-# If you want save_restore to manage its own NFS mount, specify the name and
-# IP address of the file server to which save files should be written.
-# This currently is supported only on vxWorks.
-#save_restoreSet_NFSHost("oxygen", "164.54.52.4")
+# This file does not require modification for standard use, but...
 
-# status-PV prefix
+# status PVs
+#save_restoreSet_UseStatusPVs(1)
 save_restoreSet_status_prefix("xxx:")
-# Debug-output level
-save_restoreSet_Debug(0)
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=xxx:, DEAD_SECONDS=5")
 
 # Ok to save/restore save sets with missing values (no CA connection to PV)?
 save_restoreSet_IncompleteSetsOk(1)
+
 # Save dated backup files?
 save_restoreSet_DatedBackupFiles(1)
 
 # Number of sequenced backup files to write
 save_restoreSet_NumSeqFiles(3)
+
 # Time interval between sequenced backups
 save_restoreSet_SeqPeriodInSeconds(300)
 
 # specify where save files should be
 set_savefile_path("$(STARTUP)", "autosave")
 
+###
 # specify what save files should be restored.  Note these files must be
 # in the directory specified in set_savefile_path(), or, if that function
 # has not been called, from the directory current when iocInit is invoked
@@ -31,11 +33,14 @@ set_pass0_restoreFile("auto_positions.sav")
 set_pass0_restoreFile("auto_settings.sav")
 set_pass1_restoreFile("auto_settings.sav")
 
+# Note that you can restore a .sav file without also autosaving to it.
+#set_pass0_restoreFile("myInitData.sav")
+#set_pass1_restoreFile("myInitData.sav")
+
+###
 # specify directories in which to to search for included request files
-# Note cdCommands defines 'startup', but envPaths does not
 set_requestfile_path("$(STARTUP)", "")
 set_requestfile_path("$(STARTUP)", "autosave")
-set_requestfile_path("$(TOP)", "xxxApp/Db")
 set_requestfile_path("$(AUTOSAVE)", "asApp/Db")
 set_requestfile_path("$(CALC)", "calcApp/Db")
 #set_requestfile_path("$(CAMAC)", "camacApp/Db")
@@ -47,11 +52,18 @@ set_requestfile_path("$(IP330)", "ip330App/Db")
 set_requestfile_path("$(IPUNIDIG)", "ipUnidigApp/Db")
 #set_requestfile_path("$(LOVE)", "loveApp/Db")
 set_requestfile_path("$(MCA)", "mcaApp/Db")
+#set_requestfile_path("$(MODBUS)", "modbusApp/Db")
 set_requestfile_path("$(MOTOR)", "motorApp/Db")
 set_requestfile_path("$(OPTICS)", "opticsApp/Db")
+#set_requestfile_path("$(PILATUS)", "pilatusApp/Db")
 set_requestfile_path("$(QUADEM)", "quadEMApp/Db")
 set_requestfile_path("$(SSCAN)", "sscanApp/Db")
 set_requestfile_path("$(STD)", "stdApp/Db")
+#set_requestfile_path("$(VAC)", "vacApp/Db")
 set_requestfile_path("$(VME)", "vmeApp/Db")
+set_requestfile_path("$(TOP)", "xxxApp/Db")
 
-dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=xxx:")
+# Debug-output level
+save_restoreSet_Debug(0)
+
+# END save_restore.cmd --------------------------------------------------------
