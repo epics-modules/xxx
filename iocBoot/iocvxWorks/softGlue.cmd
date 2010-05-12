a0160a79cfa02f45e4ed2719e9b24500be0c7f0b
timmmooney <mooney@aps.anl.gov>
2010-05-12 21:20:19 +0000

    Ip1k125 -> IP_EP201

--- iocBoot/iocvxWorks/softGlue.cmd
+++ iocBoot/iocvxWorks/softGlue.cmd
@@ -7,26 +7,25 @@ initIP_EP200_FPGA(0, 2, "$(SOFTGLUE)/db/EP200_FPGA.hex")
 
 # Each instance of a fieldIO_registerSet component is initialized as follows:
 #
-#int initIp1k125(const char *portName, ushort_t carrier, ushort_t slot,
-#	int msecPoll, int dataDir, int sopcOffset, int sopcVector,
+#int initIP_EP201(const char *portName, ushort_t carrier, ushort_t slot,
+#	int msecPoll, int dataDir, int sopcOffset, int interruptVector,
 #	int risingMask, int fallingMask)
-# For example:
-#initIp1k125("test1",0,2,0,0x0,  0x0 ,0x80,0,0)
-# These commands implement field I/O
-initIp1k125("sg_in1",0,2,0,0x0,  0x0 ,0x80,0,0)
-initIp1k125("sg_out1",0,2,0,0x101, 0x10 ,0x81,0,0)
+# 16 input bits
+initIP_EP201("sg_in1",0,2,0,0x0,  0x0 ,0x80,0,0)
+# 16 output bits
+initIP_EP201("sg_out1",0,2,0,0x101, 0x10 ,0x81,0,0)
 
 
 # All instances of a single-register component are initialized with a single
 # call, as follows:
 #
-#initIp1k125SingleRegisterPort(const char *portName, ushort_t carrier,
+#initIP_EP201SingleRegisterPort(const char *portName, ushort_t carrier,
 #	ushort_t slot)
 #
 # For example:
-# initIp1k125SingleRegisterPort("SOFTGLUE", 0, 2)
+# initIP_EP201SingleRegisterPort("SOFTGLUE", 0, 2)
 	
-initIp1k125SingleRegisterPort("SOFTGLUE", 0, 2)
+initIP_EP201SingleRegisterPort("SOFTGLUE", 0, 2)
 
 
 # Load a single database that all database fragments supporting single-register
