110d00d6017056d9b3734b0b4b4788232c018b0d
Pete Jemian <prjemian@gmail.com>
2011-05-20 19:35:45 +0000

    changed starter scripts (csh and bash) to make appending display path even clearer and shorter

--- start_epics_xxx.bash
+++ start_epics_xxx.bash
@@ -1,48 +1,52 @@
 #!/bin/bash
 
 EPICS_APP=/home/oxygen/MOONEY/epics/synApps_5_5/support/xxx
+EPICS_APP_ADL_DIR=${EPICS_APP}/xxxApp/op/adl
 
-output=`perl -s $EPICS_APP/release.pl -form=bash $EPICS_APP`
+output=`perl -s ${EPICS_APP}/release.pl -form=bash ${EPICS_APP}`
 eval $output
 
 #######################################
 # Prepare MEDM path
 # EDP is temporary EPICS_DISPLAY_PATH
 #
+function append_EDP { # note: in bash, macros are not expanded in aliases
+  EDP=${EDP}:$@ 
+}
 EDP=.
-EDP=$EDP:$EPICS_APP/xxxApp/op/adl
-EDP=$EDP:$AREA_DETECTOR/ADApp/op/adl
-EDP=$EDP:$AUTOSAVE/asApp/op/adl
-EDP=$EDP:$BUSY/busyApp/op/adl
-EDP=$EDP:$CALC/calcApp/op/adl
-EDP=$EDP:$CAMAC/camacApp/op/adl
-EDP=$EDP:$DAC128V/dac128VApp/op/adl
-EDP=$EDP:$DELAYGEN/delaygenApp/op/adl
-EDP=$EDP:$DXP/dxpApp/op/adl
-EDP=$EDP:$IP/ipApp/op/adl
-EDP=$EDP:$IP330/ip330App/op/adl
-EDP=$EDP:$IPUNIDIG/ipUnidigApp/op/adl
-EDP=$EDP:$LOVE/loveApp/op/adl
-EDP=$EDP:$MCA/mcaApp/op/adl
-EDP=$EDP:$MOTOR/motorApp/op/adl
-EDP=$EDP:$OPTICS/opticsApp/op/adl
-EDP=$EDP:$QUADEM/quadEMApp/op/adl
-EDP=$EDP:$SSCAN/sscanApp/op/adl
-EDP=$EDP:$STD/stdApp/op/adl
-EDP=$EDP:$VME/vmeApp/op/adl
-EDP=$EDP:$ASYN/medm
-EDP=$EDP:$VAC/vacApp/op/adl
-EDP=$EDP:$SOFTGLUE/softGlueApp/op/adl
+append_EDP  ${EPICS_APP_ADL_DIR}
+append_EDP  ${AREA_DETECTOR}/ADApp/op/adl
+append_EDP  ${AUTOSAVE}/asApp/op/adl
+append_EDP  ${BUSY}/busyApp/op/adl
+append_EDP  ${CALC}/calcApp/op/adl
+append_EDP  ${CAMAC}/camacApp/op/adl
+append_EDP  ${DAC128V}/dac128VApp/op/adl
+append_EDP  ${DELAYGEN}/delaygenApp/op/adl
+append_EDP  ${DXP}/dxpApp/op/adl
+append_EDP  ${IP}/ipApp/op/adl
+append_EDP  ${IP330}/ip330App/op/adl
+append_EDP  ${IPUNIDIG}/ipUnidigApp/op/adl
+append_EDP  ${LOVE}/loveApp/op/adl
+append_EDP  ${MCA}/mcaApp/op/adl
+append_EDP  ${MOTOR}/motorApp/op/adl
+append_EDP  ${OPTICS}/opticsApp/op/adl
+append_EDP  ${QUADEM}/quadEMApp/op/adl
+append_EDP  ${SSCAN}/sscanApp/op/adl
+append_EDP  ${STD}/stdApp/op/adl
+append_EDP  ${VME}/vmeApp/op/adl
+append_EDP  ${ASYN}/medm
+append_EDP  ${VAC}/vacApp/op/adl
+append_EDP  ${SOFTGLUE}/softGlueApp/op/adl
 
 if [ -z "$EPICS_DISPLAY_PATH" ] 
 then
-    export EPICS_DISPLAY_PATH=$EDP
+    export EPICS_DISPLAY_PATH=${EDP}
 else
-    export EPICS_DISPLAY_PATH=$EDP:$EPICS_DISPLAY_PATH
+    export EPICS_DISPLAY_PATH=${EDP}:${EPICS_DISPLAY_PATH}
 fi
-echo $EPICS_DISPLAY_PATH
+echo ${EPICS_DISPLAY_PATH}
 
-if [ -z "$MEDM_EXEC_LIST" ] 
+if [ -z "${MEDM_EXEC_LIST}" ] 
 then
     export MEDM_EXEC_LIST='Probe;probe &P &'
 fi
@@ -53,5 +57,5 @@ fi
 # see 'putenv "EPICS_CA_MAX_ARRAY_BYTES=64008"' in iocBoot/ioc<target>/st.cmd
 export EPICS_CA_MAX_ARRAY_BYTES=64008
 
-cd $EPICS_APP/xxxApp/op/adl
+cd ${EPICS_APP}/xxxApp/op/adl
 medm xxx.adl&
