f56eb702471b24bb9652ceeb12e6d9b48a11f86f
keenanlang <klang@aps.anl.gov>
2022-06-08 14:54:20 -0500

    Use silent comments to clean up IOC output

--- iocBoot/iocxxx/examples/canberra_1.cmd
+++ iocBoot/iocxxx/examples/canberra_1.cmd
@@ -6,16 +6,16 @@
 #epicsEnvSet("aimDebug", 1)
 #epicsEnvSet("icbDebug", 1)
 
-# AIMConfig(portName, ethernet_address, portNumber, maxChans,
-#           maxSignals, maxSequences, ethernetDevice)
-#    portName,         # asyn port name to be created
-#    ethernet_address, # Low order 16 bits of Ethernet hardware address
-#    portNumber,       # ADC port on AIM (1 or 2)
-#    maxChans,         # Maximum channels for this input
-#    maxSignals,       # Maximum signals for this input (>1 for MCS or multiplexor)
-#    maxSequences,     # Maximum sequences for time resolved applications
-#    ethernetDevice)   # Ethernet device name on IOC
-#                      # Typically ei0 for Motorola 68K, dc0 for ppc, eth0 for Linux
+#- AIMConfig(portName, ethernet_address, portNumber, maxChans,
+#-           maxSignals, maxSequences, ethernetDevice)
+#-    portName,         # asyn port name to be created
+#-    ethernet_address, # Low order 16 bits of Ethernet hardware address
+#-    portNumber,       # ADC port on AIM (1 or 2)
+#-    maxChans,         # Maximum channels for this input
+#-    maxSignals,       # Maximum signals for this input (>1 for MCS or multiplexor)
+#-    maxSequences,     # Maximum sequences for time resolved applications
+#-    ethernetDevice)   # Ethernet device name on IOC
+#-                      # Typically ei0 for Motorola 68K, dc0 for ppc, eth0 for Linux
 AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "dc0")
 AIMConfig("AIM1/2", 0x59e, 2, 2048, 8, 1, "dc0")
 AIMConfig("DSA2000", 0x8058, 1, 2048, 1, 1, "dc0")
@@ -26,16 +26,16 @@ dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc4,DTYP=asynMCA,IN
 dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc5,DTYP=asynMCA,INP=@asyn(AIM1/2 6),NCHAN=2048")
 dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc6,DTYP=asynMCA,INP=@asyn(DSA2000 0),NCHAN=2048")
 
-# icbConfig(portName, ethernetAddress, icbAddress, moduleType)
-#    portName to give to this asyn port
-#    ethernetAddress - Ethernet address of module, low order 16 bits
-#    icbAddress - rotary switch setting inside ICB module
-#    moduleType
-#       0 = ADC
-#       1 = Amplifier
-#       2 = HVPS
-#       3 = TCA
-#       4 = DSP
+#- icbConfig(portName, ethernetAddress, icbAddress, moduleType)
+#-    portName to give to this asyn port
+#-    ethernetAddress - Ethernet address of module, low order 16 bits
+#-    icbAddress - rotary switch setting inside ICB module
+#-    moduleType
+#-       0 = ADC
+#-       1 = Amplifier
+#-       2 = HVPS
+#-       3 = TCA
+#-       4 = DSP
 icbConfig("icbAdc1", 0x59e, 5, 0)
 dbLoadRecords("$(MCA)/mcaApp/Db/icb_adc.db", "P=$(PREFIX),ADC=adc1,PORT=icbAdc1")
 icbConfig("icbAmp1", 0x59e, 3, 1)
--- iocBoot/iocxxx/examples/canberra_13.cmd
+++ iocBoot/iocxxx/examples/canberra_13.cmd
@@ -1,16 +1,16 @@
 
 # BEGIN canberra_13.cmd -------------------------------------------------------
 
-# AIMConfig(portName, ethernet_address, portNumber, maxChans,
-#           maxSignals, maxSequences, ethernetDevice)
-#    portName,         # asyn port name to be created
-#    ethernet_address, # Low order 16 bits of Ethernet hardware address
-#    portNumber,       # ADC port on AIM (1 or 2)
-#    maxChans,         # Maximum channels for this input
-#    maxSignals,       # Maximum signals for this input (>1 for MCS or multiplexor)
-#    maxSequences,     # Maximum sequences for time resolved applications
-#    ethernetDevice)   # Ethernet device name on IOC
-#                      # Typically ei0 for Motorola 68K, dc0 for ppc, eth0 for Linux
+#- AIMConfig(portName, ethernet_address, portNumber, maxChans,
+#-           maxSignals, maxSequences, ethernetDevice)
+#-    portName,         # asyn port name to be created
+#-    ethernet_address, # Low order 16 bits of Ethernet hardware address
+#-    portNumber,       # ADC port on AIM (1 or 2)
+#-    maxChans,         # Maximum channels for this input
+#-    maxSignals,       # Maximum signals for this input (>1 for MCS or multiplexor)
+#-    maxSequences,     # Maximum sequences for time resolved applications
+#-    ethernetDevice)   # Ethernet device name on IOC
+#-                      # Typically ei0 for Motorola 68K, dc0 for ppc, eth0 for Linux
 AIMConfig("AIM2/1", 0x98c, 1, 4000, 4, 1, "ei0")
 AIMConfig("AIM2/2", 0x98d, 2, 4000, 4, 1, "ei0")
 AIMConfig("AIM3/1", 0x98b, 1, 4000, 4, 1, "ei0")
@@ -18,16 +18,16 @@ AIMConfig("AIM3/2", 0x903, 2, 4000, 4, 1, "ei0")
 
 dbLoadRecords("$(MCA)/mcaApp/Db/13element.db","P=$(PREFIX)med:")
 
-# icbConfig(portName, ethernetAddress, icbAddress, moduleType)
-#    portName to give to this asyn port
-#    ethernetAddress - Ethernet address of module, low order 16 bits
-#    icbAddress - rotary switch setting inside ICB module
-#    moduleType
-#       0 = ADC
-#       1 = Amplifier
-#       2 = HVPS
-#       3 = TCA
-#       4 = DSP
+#- icbConfig(portName, ethernetAddress, icbAddress, moduleType)
+#-    portName to give to this asyn port
+#-    ethernetAddress - Ethernet address of module, low order 16 bits
+#-    icbAddress - rotary switch setting inside ICB module
+#-    moduleType
+#-       0 = ADC
+#-       1 = Amplifier
+#-       2 = HVPS
+#-       3 = TCA
+#-       4 = DSP
 # Amp's
 icbConfig("icbAmp1", 0x98c, 1, 1)
 icbConfig("icbAmp2", 0x98c, 2, 1)
